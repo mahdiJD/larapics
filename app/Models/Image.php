c8aa6763dab1462e281b20d33b47a55d1dba6745
“[Mahdi <[bijarimahdi@gmail.com]>
2024-03-25 15:26:21 +0330

    add protected function getDimension($image){

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Image extends Model
 {
     use HasFactory;
+
+    public static function makeDirectory($subFolder){
+        $subFolder = 'images/' . date('y/m/d');
+        Storage::makeDirectory($subFolder);
+        return $subFolder;
+    }
+    public static function getDimension($image){
+        [$width,$height] = getimagesize(Storage::path($image));
+        return $width."x".$height;
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Image;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,6 +20,14 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        Image::factory(10)->create();
+
+        $images = Storage::allFiles('images');
+        foreach($images as $image){
+            Image::factory()->create([
+                'file' => $image,
+                'dimension' => Image::getDimension($image),
+            ]);
+        }
+        // Image::factory(10)->create();
     }
 }
